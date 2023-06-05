365a3e3443be193c7bbe7d736391dcb4724970e3
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2023-06-05 13:10:06 -0600

    contact banner done

---
+++ components/ContactBanner.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="flex flex-col items-center border-cyan-500 border-2 rounded-xl bg-white py-7">
+		<div class="space-y-4">
+			<h4 class="text-center text-4xl font-bold text-cyan-800">
+				Ready to get started?
+			</h4>
+			<p class="text-2xl text-neutral-800">
+				Best Tech team are not instant, born they're made...
+			</p>
+		</div>
+		<a
+			href="#"
+			class="mt-12 hover:bg-primary-800 focus:ring-primary-300 mr-3 inline-flex w-fit items-center justify-center rounded border border-primary px-5 py-2.5 text-center text-base font-medium uppercase text-primary focus:ring-4"
+		>
+			contact us
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				class="ml-2 fill-primary"
+			>
+				<path d="M3 20v-6l8-2l-8-2V4l19 8l-19 8Z" />
+			</svg>
+		</a>
+	</div>
+</template>

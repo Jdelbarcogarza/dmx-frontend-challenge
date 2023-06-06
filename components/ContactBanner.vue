f68fa6f3dea81bad4d4590e37d1e0e47f7c35832
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2023-06-06 10:01:36 -0600

    se agregaron responsive styles

--- components/ContactBanner.vue
+++ components/ContactBanner.vue
@@ -1,16 +1,16 @@
 <template>
-	<div class="flex flex-col items-center border-cyan-500 border-2 rounded-xl bg-white py-7">
-		<div class="space-y-4">
-			<h4 class="text-center text-4xl font-bold text-cyan-800">
-				Ready to get started?
-			</h4>
+	<div
+		class="flex flex-col md:flex-row w-full max-w-7xl justify-around items-center rounded-xl border-2 border-cyan-500 bg-white py-7 px-16"
+	>
+		<div class="flex flex-col justify-start space-y-4">
+			<h4 class="text-4xl font-bold text-cyan-800">Ready to get started?</h4>
 			<p class="text-2xl text-neutral-800">
 				Best Tech team are not instant, born they're made...
 			</p>
 		</div>
 		<a
 			href="#"
-			class="mt-12 hover:bg-primary-800 focus:ring-primary-300 mr-3 inline-flex w-fit items-center justify-center rounded border border-primary px-5 py-2.5 text-center text-base font-medium uppercase text-primary focus:ring-4"
+			class="hover:bg-primary-800 focus:ring-primary-300 inline-flex w-fit items-center justify-center rounded border border-primary md:px-12 md:py-4 text-center text-lg font-medium uppercase text-primary focus:ring-4 px-6 py-2 sm:mt-11"
 		>
 			contact us
 			<svg

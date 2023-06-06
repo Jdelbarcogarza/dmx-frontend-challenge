028eefbcda86a43b9c52e9b1aff869ca1868e2c8
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2023-06-06 10:35:45 -0600

    created contact card

--- components/ContactCard.vue
+++ components/ContactCard.vue
@@ -1,5 +1,55 @@
 <template>
-  <div>
-    
-  </div>
+	<div class="w-fit max-w-sm rounded-xl bg-white p-4 drop-shadow-xl">
+		<h4 class="text-xl font-medium text-neutral-800">Head Quarter</h4>
+
+		<!-- grid  -->
+		<div class="grid grid-cols-2 grid-rows-2">
+			<!-- item 1 -->
+			<div class="flex items-center">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="18"
+					height="18"
+					viewBox="0 0 24 24"
+					class="fill-primary"
+				>
+					<path
+						fill=""
+						d="M19.95 21q-3.225 0-6.287-1.438t-5.425-3.8q-2.363-2.362-3.8-5.425T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.225t.325.575l.65 3.5q.05.35-.013.638T9.4 8.45L7 10.9q1.05 1.8 2.625 3.375T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.075.575.338T21 15.9v4.05q0 .45-.3.75t-.75.3Z"
+					/>
+				</svg>
+				<p class="ml-2">+123 456 89 79</p>
+			</div>
+
+			<!-- item 2 -->
+			<div class="flex items-center">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="18"
+					height="18"
+					viewBox="0 0 24 24"
+					class="fill-primary"
+				>
+					<path d="M2 20V4h20v16H2Zm10-7l8-5V6l-8 5l-8-5v2l8 5Z" />
+				</svg>
+				<p class="ml-2">hello@ipsum.com</p>
+			</div>
+
+			<!-- item 3 -->
+			<div class="col-span-2 flex items-center">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="18"
+					height="18"
+					viewBox="0 0 24 24"
+					class="fill-primary"
+				>
+					<path
+						d="M12 12q.825 0 1.413-.588T14 10q0-.825-.588-1.413T12 8q-.825 0-1.413.588T10 10q0 .825.588 1.413T12 12Zm0 10q-4.025-3.425-6.012-6.362T4 10.2q0-3.75 2.413-5.975T12 2q3.175 0 5.588 2.225T20 10.2q0 2.5-1.988 5.438T12 22Z"
+					/>
+				</svg>
+				<p class="ml-2">Lorem ipsum dolor sit amet. Lorem, ipsum dolor.</p>
+			</div>
+		</div>
+	</div>
 </template>

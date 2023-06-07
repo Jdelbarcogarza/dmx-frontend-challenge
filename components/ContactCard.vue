d26ac2e29db1734f92f2bdf5d59f4c6cd1cde6c9
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2023-06-07 11:25:58 -0600

    made contact cards responsive

--- components/ContactCard.vue
+++ components/ContactCard.vue
@@ -1,38 +1,42 @@
 <template>
-	<div class="w-fit max-w-sm rounded-xl bg-white p-4 drop-shadow-xl">
-		<h4 class="text-xl font-medium text-neutral-800">Head Quarter</h4>
+	<div class="h-fit w-fit max-w-lg rounded-xl bg-white p-4 drop-shadow-lg">
+		<h4 class="text-xl font-medium text-neutral-800 pb-4">Head Quarter</h4>
 
 		<!-- grid  -->
-		<div class="grid grid-cols-2 grid-rows-2">
-			<!-- item 1 -->
-			<div class="flex items-center">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="18"
-					height="18"
-					viewBox="0 0 24 24"
-					class="fill-primary"
-				>
-					<path
-						fill=""
-						d="M19.95 21q-3.225 0-6.287-1.438t-5.425-3.8q-2.363-2.362-3.8-5.425T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.225t.325.575l.65 3.5q.05.35-.013.638T9.4 8.45L7 10.9q1.05 1.8 2.625 3.375T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.075.575.338T21 15.9v4.05q0 .45-.3.75t-.75.3Z"
-					/>
-				</svg>
-				<p class="ml-2">+123 456 89 79</p>
-			</div>
+		<div class="grid grid-cols-1 grid-rows-2 md:gap-4 gap-2">
 
-			<!-- item 2 -->
-			<div class="flex items-center">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="18"
-					height="18"
-					viewBox="0 0 24 24"
-					class="fill-primary"
-				>
-					<path d="M2 20V4h20v16H2Zm10-7l8-5V6l-8 5l-8-5v2l8 5Z" />
-				</svg>
-				<p class="ml-2">hello@ipsum.com</p>
+			<!-- flex container -->
+			<div class="flex justify-between flex-col md:flex-row space-y-3 md:space-y-0">
+				<!-- item 1 -->
+				<div class="flex items-center w-1/2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="18"
+						height="18"
+						viewBox="0 0 24 24"
+						class="fill-primary"
+					>
+						<path
+							fill=""
+							d="M19.95 21q-3.225 0-6.287-1.438t-5.425-3.8q-2.363-2.362-3.8-5.425T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.225t.325.575l.65 3.5q.05.35-.013.638T9.4 8.45L7 10.9q1.05 1.8 2.625 3.375T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.075.575.338T21 15.9v4.05q0 .45-.3.75t-.75.3Z"
+						/>
+					</svg>
+					<p class="ml-2">+123 456 89 79</p>
+				</div>
+
+				<!-- item 2 -->
+				<div class="flex items-center w-1/2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="18"
+						height="18"
+						viewBox="0 0 24 24"
+						class="fill-primary"
+					>
+						<path d="M2 20V4h20v16H2Zm10-7l8-5V6l-8 5l-8-5v2l8 5Z" />
+					</svg>
+					<p class="ml-2">hello@ipsum.com</p>
+				</div>
 			</div>
 
 			<!-- item 3 -->

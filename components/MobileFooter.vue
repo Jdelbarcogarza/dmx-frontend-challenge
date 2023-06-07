ace7abe595444002dd5d1d4d12c5130ce488cba6
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2023-06-07 13:12:36 -0600

    accordion behaves good on mobile

--- components/MobileFooter.vue
+++ components/MobileFooter.vue
@@ -1,60 +1,200 @@
 <template>
-	<div  id="accordion-collapse" data-accordion="collapse" class="mt-11">
-		<AccordionItem  :item-number="1" title="Company">
-			<ul class="space-y-5 text-stone-400">
-				<li>
-					<NuxtLink to="#">Team</NuxtLink>
-				</li>
-				<li>
-					<NuxtLink to="#">History</NuxtLink>
-				</li>
-        <li>
-					<NuxtLink to="#">Contact us</NuxtLink>
-				</li>
-        <li>
-					<NuxtLink to="#">Locations</NuxtLink>
-				</li>
-			</ul>
-		</AccordionItem>
+	<div class="py-4">
+		<div class="flex justify-center">
+			<div id="accordion-collapse" data-accordion="collapse" class="mt-11 w-full max-w-lg">
+				<AccordionItem :item-number="1" title="Company">
+					<ul class="space-y-5 text-stone-400">
+						<li>
+							<NuxtLink to="#">Team</NuxtLink>
+						</li>
+						<li>
+							<NuxtLink to="#">History</NuxtLink>
+						</li>
+						<li>
+							<NuxtLink to="#">Contact us</NuxtLink>
+						</li>
+						<li>
+							<NuxtLink to="#">Locations</NuxtLink>
+						</li>
+					</ul>
+				</AccordionItem>
 
-		<AccordionItem :item-number="2" title="Resources">
-      <ul class="space-y-5 text-stone-400">
-				<li>
-					<NuxtLink to="#">Resource</NuxtLink>
-				</li>
-				<li>
-					<NuxtLink to="#">Resource name</NuxtLink>
-				</li>
-        <li>
-					<NuxtLink to="#">Another resource</NuxtLink>
-				</li>
-        <li>
-					<NuxtLink to="#">Final resource</NuxtLink>
-				</li>
-			</ul>
-		</AccordionItem>
+				<AccordionItem :item-number="2" title="Resources">
+					<ul class="space-y-5 text-stone-400">
+						<li>
+							<NuxtLink to="#">Resource</NuxtLink>
+						</li>
+						<li>
+							<NuxtLink to="#">Resource name</NuxtLink>
+						</li>
+						<li>
+							<NuxtLink to="#">Another resource</NuxtLink>
+						</li>
+						<li>
+							<NuxtLink to="#">Final resource</NuxtLink>
+						</li>
+					</ul>
+				</AccordionItem>
 
-    <AccordionItem :item-number="3" title="Legal">
-      <ul class="space-y-5 text-stone-400">
-				<li>
-					<NuxtLink to="#">Privacy policy</NuxtLink>
-				</li>
-				<li>
-					<NuxtLink to="#">Terms of use</NuxtLink>
-				</li>
-			</ul>
-		</AccordionItem>
+				<AccordionItem :item-number="3" title="Legal">
+					<ul class="space-y-5 text-stone-400">
+						<li>
+							<NuxtLink to="#">Privacy policy</NuxtLink>
+						</li>
+						<li>
+							<NuxtLink to="#">Terms of use</NuxtLink>
+						</li>
+					</ul>
+				</AccordionItem>
+			</div>
+		</div>
+
+		<!-- social icons -->
+
+		<div class="mt-8 flex justify-center space-x-4 py-4">
+			<SocialIcon>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 16 16"
+					class="h-full w-full fill-blue-800"
+				>
+					<path
+						fill=""
+						d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334c0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518a3.301 3.301 0 0 0 1.447-1.817a6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429a3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218a3.203 3.203 0 0 1-.865.115a3.23 3.23 0 0 1-.614-.057a3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"
+					/>
+				</svg>
+			</SocialIcon>
+
+			<SocialIcon>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+					class="h-full w-full fill-blue-800"
+				>
+					<path
+						fill=""
+						d="M15.402 21v-6.966h2.333l.349-2.708h-2.682V9.598c0-.784.218-1.319 1.342-1.319h1.434V5.857a19.19 19.19 0 0 0-2.09-.107c-2.067 0-3.482 1.262-3.482 3.58v1.996h-2.338v2.708h2.338V21H4a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1h-4.598Z"
+					/>
+				</svg>
+			</SocialIcon>
+
+			<SocialIcon>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					class="h-full w-full fill-blue-800"
+					viewBox="0 0 24 24"
+				>
+					<path
+						d="M13.028 2.001a78.82 78.82 0 0 1 2.189.022l.194.007c.224.008.445.018.712.03c1.064.05 1.79.218 2.427.465c.66.254 1.216.598 1.772 1.154a4.908 4.908 0 0 1 1.153 1.771c.247.637.415 1.364.465 2.428c.012.266.022.488.03.712l.006.194a79 79 0 0 1 .023 2.188l.001.746v1.31a78.836 78.836 0 0 1-.023 2.189l-.006.194c-.008.224-.018.445-.03.712c-.05 1.064-.22 1.79-.466 2.427a4.884 4.884 0 0 1-1.153 1.772a4.915 4.915 0 0 1-1.772 1.153c-.637.247-1.363.415-2.427.465c-.267.012-.488.022-.712.03l-.194.006a79 79 0 0 1-2.189.023l-.746.001h-1.309a78.836 78.836 0 0 1-2.189-.023l-.194-.006a60.64 60.64 0 0 1-.712-.03c-1.064-.05-1.79-.22-2.428-.466a4.89 4.89 0 0 1-1.771-1.153a4.904 4.904 0 0 1-1.154-1.772c-.247-.637-.415-1.363-.465-2.427a74.367 74.367 0 0 1-.03-.712l-.005-.194A79.053 79.053 0 0 1 2 13.028v-2.056a78.82 78.82 0 0 1 .022-2.188l.007-.194c.008-.224.018-.446.03-.712c.05-1.065.218-1.79.465-2.428A4.88 4.88 0 0 1 3.68 3.68a4.897 4.897 0 0 1 1.77-1.155c.638-.247 1.363-.415 2.428-.465l.712-.03l.194-.005A79.053 79.053 0 0 1 10.972 2h2.056Zm-1.028 5A5 5 0 1 0 12 17a5 5 0 0 0 0-10Zm0 2A3 3 0 1 1 12.001 15a3 3 0 0 1 0-6Zm5.25-3.5a1.25 1.25 0 0 0 0 2.498a1.25 1.25 0 0 0 0-2.5Z"
+					/>
+				</svg>
+			</SocialIcon>
+
+			<SocialIcon>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 24 24"
+					class="h-full w-full fill-blue-800"
+				>
+					<path
+						d="M18.336 18.339h-2.665v-4.177c0-.996-.02-2.278-1.39-2.278c-1.389 0-1.601 1.084-1.601 2.205v4.25h-2.666V9.75h2.56v1.17h.035c.358-.674 1.228-1.387 2.528-1.387c2.7 0 3.2 1.778 3.2 4.091v4.715ZM7.004 8.575a1.546 1.546 0 0 1-1.548-1.549a1.548 1.548 0 1 1 1.547 1.549Zm1.336 9.764H5.667V9.75H8.34v8.589ZM19.67 3H4.33C3.594 3 3 3.58 3 4.297v15.406C3 20.42 3.594 21 4.328 21h15.339C20.4 21 21 20.42 21 19.703V4.297C21 3.581 20.4 3 19.666 3h.003Z"
+					/>
+				</svg>
+			</SocialIcon>
+		</div>
+
+		<div class="mt-4 flex justify-center">
+			<button
+				id="dropdownTopButton"
+				data-dropdown-toggle="dropdownTop"
+				data-dropdown-placement="top"
+				class="mb-3 mr-3 inline-flex items-center rounded border border-blue-800 bg-white px-4 py-2.5 text-center font-medium text-blue-800 focus:outline-none focus:ring-4 md:mb-0"
+				type="button"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="32"
+					height="32"
+					viewBox="0 0 16 16"
+					class="mr-2 h-4 w-4 fill-blue-800"
+				>
+					<path
+						d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm7.5-6.923c-.67.204-1.335.82-1.887 1.855A7.97 7.97 0 0 0 5.145 4H7.5V1.077zM4.09 4a9.267 9.267 0 0 1 .64-1.539a6.7 6.7 0 0 1 .597-.933A7.025 7.025 0 0 0 2.255 4H4.09zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a6.958 6.958 0 0 0-.656 2.5h2.49zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5H4.847zM8.5 5v2.5h2.99a12.495 12.495 0 0 0-.337-2.5H8.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5H4.51zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5H8.5zM5.145 12c.138.386.295.744.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12H5.145zm.182 2.472a6.696 6.696 0 0 1-.597-.933A9.268 9.268 0 0 1 4.09 12H2.255a7.024 7.024 0 0 0 3.072 2.472zM3.82 11a13.652 13.652 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5H3.82zm6.853 3.472A7.024 7.024 0 0 0 13.745 12H11.91a9.27 9.27 0 0 1-.64 1.539a6.688 6.688 0 0 1-.597.933zM8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855c.173-.324.33-.682.468-1.068H8.5zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.65 13.65 0 0 1-.312 2.5zm2.802-3.5a6.959 6.959 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5h2.49zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7.024 7.024 0 0 0-3.072-2.472c.218.284.418.598.597.933zM10.855 4a7.966 7.966 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4h2.355z"
+					/>
+				</svg>
+				English
+				<svg
+					class="ml-8 h-4 w-4"
+					aria-hidden="true"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
+						clip-rule="evenodd"
+					></path>
+				</svg>
+			</button>
+
+			<div
+				id="dropdownTop"
+				class="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
+			>
+				<ul
+					class="py-2 text-sm text-gray-700 dark:text-gray-200"
+					aria-labelledby="dropdownTopButton"
+				>
+					<li>
+						<a
+							href="#"
+							class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>Dashboard</a
+						>
+					</li>
+					<li>
+						<a
+							href="#"
+							class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>Settings</a
+						>
+					</li>
+					<li>
+						<a
+							href="#"
+							class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>Earnings</a
+						>
+					</li>
+					<li>
+						<a
+							href="#"
+							class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+							>Sign out</a
+						>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-import { initAccordions } from "flowbite";
+import { initAccordions, initDropdowns } from "flowbite";
 import AccordionItem from "./UI/AccordionItem.vue";
-
-const dropdown = ref();
+import SocialIcon from "./UI/SocialIcon.vue";
 
 onMounted(() => {
-
 	initAccordions();
+	initDropdowns();
 });
 </script>

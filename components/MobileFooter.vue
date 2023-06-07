2cd0b2b7f977ae0f0f8f694693a6a610859ea3fb
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2023-06-07 11:58:19 -0600

    implemented mobile footer component

---
+++ components/MobileFooter.vue
@@ -0,0 +1,57 @@
+<template>
+	<div id="accordion-collapse" data-accordion="collapse" class="mt-11">
+		<AccordionItem :item-number="1" title="Company">
+			<ul class="space-y-5 text-stone-400">
+				<li>
+					<NuxtLink to="#">Team</NuxtLink>
+				</li>
+				<li>
+					<NuxtLink to="#">History</NuxtLink>
+				</li>
+        <li>
+					<NuxtLink to="#">Contact us</NuxtLink>
+				</li>
+        <li>
+					<NuxtLink to="#">Locations</NuxtLink>
+				</li>
+			</ul>
+		</AccordionItem>
+
+		<AccordionItem :item-number="2" title="Resources">
+      <ul class="space-y-5 text-stone-400">
+				<li>
+					<NuxtLink to="#">Resource</NuxtLink>
+				</li>
+				<li>
+					<NuxtLink to="#">Resource name</NuxtLink>
+				</li>
+        <li>
+					<NuxtLink to="#">Another resource</NuxtLink>
+				</li>
+        <li>
+					<NuxtLink to="#">Final resource</NuxtLink>
+				</li>
+			</ul>
+		</AccordionItem>
+
+    <AccordionItem :item-number="3" title="Legal">
+      <ul class="space-y-5 text-stone-400">
+				<li>
+					<NuxtLink to="#">Privacy policy</NuxtLink>
+				</li>
+				<li>
+					<NuxtLink to="#">Terms of use</NuxtLink>
+				</li>
+			</ul>
+		</AccordionItem>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { initAccordions } from "flowbite";
+import AccordionItem from "./UI/AccordionItem.vue";
+
+onMounted(() => {
+	initAccordions();
+});
+</script>

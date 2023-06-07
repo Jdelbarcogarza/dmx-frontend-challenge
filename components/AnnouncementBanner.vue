12faed2e3c30081f9ca5a594c6d4b10b39f6982f
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2023-06-07 13:50:34 -0600

    added anouncement banner

---
+++ components/AnnouncementBanner.vue
@@ -0,0 +1,16 @@
+<!-- left-1/2  -->
+<template>
+	<div
+		class="fixed bottom-0 index-x-0 mx-auto flex max-w-6xl items-center rounded bg-stone-700 px-4 py-3"
+	>
+		<p class="text-white">
+			Dies ist ein Beispiel für eine Nachricht, die häufig für wichtige
+			Informationen für Besucher oder Benutzer verwendet wird. Bitte klicken Sie
+			auf die Schaltfläche neben, um diese Nachricht zu schließen.
+		</p>
+
+		<button class="h-fit rounded bg-white px-10 py-1.5 uppercase">
+			Accept
+		</button>
+	</div>
+</template>

b40b856066580f05910b14eb12fdfe4dcb5f5706
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2023-06-08 09:17:13 -0600

    announcement banner

--- components/AnnouncementBanner.vue
+++ components/AnnouncementBanner.vue
@@ -1,16 +1,31 @@
 <!-- left-1/2  -->
 <template>
-	<div
-		class="fixed bottom-0 index-x-0 mx-auto flex max-w-6xl items-center rounded bg-stone-700 px-4 py-3"
-	>
-		<p class="text-white">
-			Dies ist ein Beispiel für eine Nachricht, die häufig für wichtige
-			Informationen für Besucher oder Benutzer verwendet wird. Bitte klicken Sie
-			auf die Schaltfläche neben, um diese Nachricht zu schließen.
-		</p>
+	
+		<div
+		v-show="showBanner"
+			class="fixed bottom-0 left-1/3 z-50 mx-auto flex max-w-6xl -translate-x-1/3 items-center rounded bg-stone-700 px-4 py-3"
+		>
+			<p class="text-white">
+				Dies ist ein Beispiel für eine Nachricht, die häufig für wichtige
+				Informationen für Besucher oder Benutzer verwendet wird. Bitte klicken
+				Sie auf die Schaltfläche neben, um diese Nachricht zu schließen.
+			</p>
 
-		<button class="h-fit rounded bg-white px-10 py-1.5 uppercase">
-			Accept
-		</button>
-	</div>
+			<button @click="changeBanner()" class="ml-2 h-fit rounded bg-white px-10 py-1.5 uppercase">
+				Accept
+			</button>
+		</div>
+
 </template>
+
+
+<script setup lang="ts">
+
+const showBanner = ref<boolean>(true);
+
+
+const changeBanner = () => {
+	showBanner.value = false
+}
+
+</script>

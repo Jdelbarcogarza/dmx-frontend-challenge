4e7c67d0a6f0d42b6fe2b12021c0f1f01f5b7182
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2023-06-08 10:37:31 -0600

    fixed flowbite js bugs wit drawer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-icon'],
-  css: ['~/assets/css/main.css'],
+	app: {
+		head: {
+
+			link: [
+				{
+					rel: "stylesheet",
+					href: "https://unpkg.com/flowbite@1.4.5/dist/flowbite.min.css",
+				},
+			],
+			script: [{ src: "https://unpkg.com/flowbite@1.4.5/dist/flowbite.js" }],
+		},
+	},
+
+	modules: ["nuxt-icon"],
+	css: ["~/assets/css/main.css"],
 	plugins: ["~/plugins/autoAnimatePlugin.ts"],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-})
+	postcss: {
+		plugins: {
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+});

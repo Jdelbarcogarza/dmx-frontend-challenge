5a540a758555eb4d428bb7f4d1d0628a571e6eb9
Jorge Del Barco Garza <jdelbarcogarza@gmail.com>
2023-06-08 09:19:20 -0600

    changed color of text

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -39,7 +39,7 @@
 						<li>
 							<NuxtLink
 								to="/"
-								class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
+								class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
 								aria-current="page"
 								>About</NuxtLink
 							>
